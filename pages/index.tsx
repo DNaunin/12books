55bd6e657fa892232af99245cbda06f5ef433af0
dnaunin <doerte.naunin@baqend.com>
2022-01-12 20:36:42 +0100

    Edit dependencies and format main Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,37 @@ import Button from "../components/button/Button";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  function submitPlans() {
+    console.log(document.getElementById("planButton"))
+  }; 
+  function submitDone() {
+    console.log(document.getElementById("finButton"))
+  }; 
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>12 Books</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Button primary label="Primary" />
-        <Button primary={false} label="Secondary" />
+        <h1>Books I plan to read</h1>
+        <label>Enter your planned books here: </label>
+        <input type="text" id="plannedReads"  placeholder="plan"></input>
+        <button onClick={() => submitPlans()} id="planButton" type="button">Submit</button>
+        <ul>
+          <li>
+            Neil Gaiman - Neverwhere
+          </li>
+          <li>
+            Monstress - Marjorie Liu
+          </li>
+        </ul>
+        <h1>Books I have read</h1>
+        <label>Enter your finished reads here: </label>
+        <input type="text" id="finishedReads" placeholder="done"></input>
+        <button onClick={() => submitDone()} id="finButton" type="button">Submit</button>
       </main>
     </div>
   );
