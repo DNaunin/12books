2d2fc5ed346422ad7dfc68d158f60c83e8363c76
dnaunin <doerte.naunin@baqend.com>
2022-06-18 18:13:03 +0200

    add proptypes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
 import Head from "next/head";
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 import styles from "../styles/Home.module.css";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const bookList = [
   {
     id: 1,
-    name: 'Neverwhere',
+    name: "Neverwhere",
   },
   {
     id: 2,
-    name: 'Monstress',
+    name: "Monstress",
   },
 ];
 
 export default function Home() {
-
   const [list, setList] = React.useState(bookList);
-  const [name, setName] = React.useState('');
+  const [name, setName] = React.useState("");
 
   function handleChange(event) {
     setName(event.target.value);
@@ -28,27 +28,22 @@ export default function Home() {
 
     setList(newList);
 
-    setName('');
+    setName("");
   }
 
   return (
-    
     <div className={styles.main}>
-            <Head>
+      <Head>
         <title>12 Books</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Books to read</h1>
-      <AddItem
-        name={name}
-        onChange={handleChange}
-        onAdd={handleAdd}
-      />
+      <AddItem name={name} onChange={handleChange} onAdd={handleAdd} />
 
       <List list={list} />
     </div>
   );
-};
+}
 
 const AddItem = ({ name, onChange, onAdd }) => (
   <div className={styles.container}>
@@ -59,6 +54,12 @@ const AddItem = ({ name, onChange, onAdd }) => (
   </div>
 );
 
+AddItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onAdd: PropTypes.func.isRequired,
+};
+
 const List = ({ list }) => (
   <ul className={styles.list}>
     {list.map((item) => (
@@ -67,3 +68,6 @@ const List = ({ list }) => (
   </ul>
 );
 
+List.propTypes = {
+  list: PropTypes.array.isRequired,
+};
